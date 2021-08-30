1e13f9fd683f8b2090e8c5f3e1ed9e13556fb254
jckr <jckr.github@gmail.com>
2021-08-30 18:10:01 -0500

    added a file input, and some actions to load a file and run arbitrary sql

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import {useDispatch, useSelector} from 'react-redux';
 import "./styles.css";
 import SQLRepl from './components/sql-repl';
-import initSqlJs from "sql.js";
-
+import FileInput from "./components/file-input";
+import {initSQL} from './store/actions';
 // Required to let webpack 4 know it needs to copy the wasm file to our assets
 import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
 
 export default function App() {
-  const [db, setDb] = useState(null);
-  const [error, setError] = useState(null);
-
+  const dispatch = useDispatch();
+  const ready = useSelector((state) => state.ready);
+  const error = useSelector((state) => state.error);
   useEffect(async () => {
     // sql.js needs to fetch its wasm file, so we cannot immediately instantiate the database
     // without any configuration, initSqlJs will fetch the wasm files directly from the same path as the js
     // see ../craco.config.js
-    try {
-      const SQL = await initSqlJs({ locateFile: () => sqlWasm });
-      setDb(new SQL.Database());
-    } catch (err) {
-      setError(err);
-    }
+    dispatch(initSQL());
   }, []);
 
   if (error) return <pre>{error.toString()}</pre>;
-  else if (!db) return <pre>Loading...</pre>;
-  else return <SQLRepl db={db} />;
+  if (!ready) return <pre>Loading...</pre>;
+  return (<>
+    <FileInput />
+    <SQLRepl />
+  </>);
 }
+
