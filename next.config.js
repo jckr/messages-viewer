800968483627d1b4abd44db44239b0c64fc964cf
jckr <jeromecukier@jeromes-mbp.lan>
2023-02-24 15:59:53 -0800

    added toy SQL interpreter that works with next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer, webpack }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+
+    return config;
+  },
+};
